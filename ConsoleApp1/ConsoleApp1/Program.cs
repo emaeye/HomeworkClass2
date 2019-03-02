d16614395af35649691155b8ac4dc76259b7a7f5
emaeye <45636673+emaeye@users.noreply.github.com>
2019-03-02 20:06:02 +0100

    HomeWork N0: 1
    Emilija Filiposka Velichkovska G5
    HomeWork Class2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,54 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int number = 10;
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.White;
+            //Start:
+            Console.WriteLine("Welcome to Real Calculator! press Enter to begin");
+            Console.ReadLine();
+           
+            Console.WriteLine("Please insert one of the following operators: +, -, *, /");
+            string input = Console.ReadLine();
+            Console.WriteLine("Please insert first number");
+            int num1;
+            bool checkNumber1 = Int32.TryParse(Console.ReadLine(),out num1);
+            Console.WriteLine("Please insert second number");
+            int num2;
+            bool checkNumber2 = Int32.TryParse(Console.ReadLine(), out num2);
+            if (!checkNumber1 || !checkNumber2)
+            {
+                Console.WriteLine("Invalid input number please try again - Calculations ENDs");
+                //goto Start;
+            }
+            
+            switch (input)
+            {
+                case "+":
+                    Console.WriteLine("The result is: " + (num1 + num2));
+                    break;
+
+                case "-":
+                    Console.WriteLine("The result is: " + (num1 - num2));
+                    break;
+
+                case "*":
+                    Console.WriteLine("The result is: " + (num1 * num2));
+                    break;
+
+                case "/":
+                    Console.WriteLine("The result is: " + (num1 / num2));
+                    break;
+
+                default :
+                    Console.WriteLine("Invaild operator selected, please try again - Calculation ENDs ");
+                    //goto Start;
+                    break;
+                    
+            }
+
+            Console.ReadLine();        
         }
+
     }
 }
