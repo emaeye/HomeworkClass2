cdb579798a04f6c029273537421767faace440e1
emaeye <45636673+emaeye@users.noreply.github.com>
2019-03-03 15:39:55 +0100

    Task1 finsihed
    Prosiren e kodot na incijalnata zadaca.
    Emilija Filiposka Velichkovska
    G5

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,50 +10,114 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            
             Console.BackgroundColor = ConsoleColor.DarkGreen;
             Console.ForegroundColor = ConsoleColor.White;
+            bool correctInput = true;
+            string decision;
+            string[] result = new string[] {};
+            int index = 0;
             //Start:
-            Console.WriteLine("Welcome to Real Calculator! press Enter to begin");
-            Console.ReadLine();
+            while (correctInput)
+            {
+
+                Array.Resize(ref result, result.Length + 1);
+                Console.WriteLine("Welcome to Real Calculator! press Enter to begin");
+                Console.ReadLine();
            
-            Console.WriteLine("Please insert one of the following operators: +, -, *, /");
-            string input = Console.ReadLine();
-            Console.WriteLine("Please insert first number");
-            int num1;
-            bool checkNumber1 = Int32.TryParse(Console.ReadLine(),out num1);
-            Console.WriteLine("Please insert second number");
-            int num2;
-            bool checkNumber2 = Int32.TryParse(Console.ReadLine(), out num2);
-            if (!checkNumber1 || !checkNumber2)
-            {
-                Console.WriteLine("Invalid input number please try again - Calculations ENDs");
-                //goto Start;
-            }
-            
-            switch (input)
-            {
-                case "+":
-                    Console.WriteLine("The result is: " + (num1 + num2));
-                    break;
+                Console.WriteLine("Please insert one of the following operators: +, -, *, /");
+                string input = Console.ReadLine();
+                Console.WriteLine("Please insert first number");
+                int num1;
+                bool checkNumber1 = Int32.TryParse(Console.ReadLine(),out num1);
+                Console.WriteLine("Please insert second number");
+                int num2;
+                bool checkNumber2 = Int32.TryParse(Console.ReadLine(), out num2);
+                if ((!checkNumber1 || !checkNumber2) || (input == "/" && num2 == 0 ))
+                {
+                    Console.WriteLine("Invalid input number or you are trying to divide with 0, please try again ");
+                    continue;
+               
+                }
 
-                case "-":
-                    Console.WriteLine("The result is: " + (num1 - num2));
-                    break;
+                switch (input)
+                {
+                    case "+":
+                        Console.WriteLine("The result is: " + (num1 + num2));
+                        result[index] = num1 + "+" + num2 + "=" + (num1 + num2);
+                        index++;
+                        Console.WriteLine("To continue with calculations press Y and to abort press N");
+                        decision = Console.ReadLine();
+                        if (decision == "N")
+                        {
+                            Console.WriteLine("Calculations END's with your calculation history");
+                            for (int i=0; i<result.Length; i++)
+                            {
+                                Console.WriteLine(result[i]);
+                            }
+                            correctInput = false;
+                            break;
+                        }
+                        continue;
+                    case "-":
+                        
+                        Console.WriteLine("The result is: " + (num1 - num2));
+                        result[index] = num1 + "-" + num2 + "=" + (num1 - num2);
+                        index++;
+                        Console.WriteLine("To continue with calculations press Y and to abort press N");
+                        decision = Console.ReadLine();
+                        if (decision == "N")
+                        {
+                            Console.WriteLine("Calculations END's with your calculation history");
+                            for (int i = 0; i < result.Length; i++)
+                            {
+                                Console.WriteLine(result[i]);
+                            }
+                            correctInput = false;
+                            break;
+                        }
+                        continue;
 
-                case "*":
-                    Console.WriteLine("The result is: " + (num1 * num2));
-                    break;
+                    case "*":
+                        Console.WriteLine("The result is: " + (num1 * num2));
+                        result[index] = num1 + "*" + num2 + "=" + (num1 * num2);
+                        index++;
+                        Console.WriteLine("To continue with calculations press Y and to abort press N");
+                        decision = Console.ReadLine();
+                        if (decision == "N")
+                        {
+                            Console.WriteLine("Calculations END's with your calculation history");
+                            for (int i = 0; i < result.Length; i++)
+                            {
+                                Console.WriteLine(result[i]);
+                            }
+                            correctInput = false;
+                            break;
+                        }
+                        continue;
 
-                case "/":
-                    Console.WriteLine("The result is: " + (num1 / num2));
-                    break;
+                    case "/":
+                        Console.WriteLine("The result is: " + (num1 / num2));
+                        result[index] = num1 + "/" + num2 + "=" + (num1 / num2);
+                        index++;
+                        Console.WriteLine("To continue with calculations press Y and to abort press N");
+                        decision = Console.ReadLine();
+                        if (decision == "N")
+                        {
+                            Console.WriteLine("Calculations END's with your calculation history");
+                            for (int i = 0; i < result.Length; i++)
+                            {
+                                Console.WriteLine(result[i]);
+                            }
+                            correctInput = false;
+                            break;
+                        }
+                        continue;
 
-                default :
-                    Console.WriteLine("Invaild operator selected, please try again - Calculation ENDs ");
-                    //goto Start;
-                    break;
-                    
+                    default:
+                        Console.WriteLine("Invaild operator selected, please try again ");
+                        continue;
+                }
             }
 
             Console.ReadLine();        
